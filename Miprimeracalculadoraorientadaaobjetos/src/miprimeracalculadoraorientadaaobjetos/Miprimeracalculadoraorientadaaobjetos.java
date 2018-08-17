cead2718097b8e0c37dcc1f4b442d1292901b465
Estudiantes <Estudiantes@labing50423.udistrital.edu.co>
2018-08-17 11:55:26 -0500

    Calculadora Evelin Sola V 1.4

--- Miprimeracalculadoraorientadaaobjetos/src/miprimeracalculadoraorientadaaobjetos/Miprimeracalculadoraorientadaaobjetos.java
+++ Miprimeracalculadoraorientadaaobjetos/src/miprimeracalculadoraorientadaaobjetos/Miprimeracalculadoraorientadaaobjetos.java
@@ -18,10 +18,13 @@ public class Miprimeracalculadoraorientadaaobjetos {
         Scanner entrada = new Scanner(System.in);//sin esta cuestion no se pueden meter datos!!!
         //definir objeto
         calculadoraAritmetica miCalculadora;
-        // crear objeto
         miCalculadora=new calculadoraAritmetica();
+        calculadoratrigonometrica miCalculadora1;                                    // crear objeto
+        miCalculadora1=new calculadoratrigonometrica();
         miCalculadora.meterNumeros();
-        System.out.print("selecciones operacion:\n1.sumar\n2.restar\n3.multiplicar\n4.dividir\n5.residuo"
+        miCalculadora1.meterNumeros1();
+        
+        System.out.print("selecciones operacion:\n1.sumar\n2.restar\n3.multiplicar\n4.dividir\n5.residuo\n6.seno\n7.coseno\n8.tangente\n9.arcoseno\n10.arccoseno\n11.arctangente"
                 + "\n digite su respuesta :");
          
         int opcion=entrada.nextInt();
@@ -42,7 +45,20 @@ public class Miprimeracalculadoraorientadaaobjetos {
                    break; 
             
             case 5:System.out.println("el residuo es ="+miCalculadora.residuo());      
+                   break;  
+            case 6:System.out.println("el seno es ="+miCalculadora1.seno());      
+                   break; 
+            case 7:System.out.println("el coseno es ="+miCalculadora1.coseno());      
+                   break; 
+            case 8:System.out.println("la tangente es ="+miCalculadora1.tangente());      
                    break;        
+            case 9:System.out.println("el arcoseno es ="+miCalculadora1.arcsin());      
+                   break;     
+            case 10:System.out.println("el arccoseno es ="+miCalculadora1.arcos());      
+                   break;            
+            case 11:System.out.println("el arctangente es ="+miCalculadora1.arctan());      
+                   break;            
+                   
             default :System.out.println("opcion invalida ");  
                    break;
                   }
