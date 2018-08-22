e2256ca6b29a88d858130b01ae10415116ea5e5b
Estudiantes <Estudiantes@Labing70611.udistrital.edu.co>
2018-08-22 17:05:33 -0500

    version2.0

--- Miprimeracalculadoraorientadaaobjetos/src/miprimeracalculadoraorientadaaobjetos/calculadoratrigonometrica.java
+++ Miprimeracalculadoraorientadaaobjetos/src/miprimeracalculadoraorientadaaobjetos/calculadoratrigonometrica.java
@@ -12,11 +12,29 @@ import static java.lang.Math.*;
 public class calculadoratrigonometrica {
     double call;
     double inversa;
-}
 
-
+    void convertir (double unangulo){
+        call = Math.toRadians(unangulo);
+    }
 
  double seno(){
+     return sin(call);
+ }
+ double coseno(){
+     return cos(call);
+ }
+double tangente(){
+     return tan(call);
+ }
+double arcsin(){
+     return Math.asin(inversa);
+ }
+double arccos(){
+     return Math.acos(inversa);
+ }
+double arctan(){
+     return Math.atan(inversa);
+ }
 
 
 }
